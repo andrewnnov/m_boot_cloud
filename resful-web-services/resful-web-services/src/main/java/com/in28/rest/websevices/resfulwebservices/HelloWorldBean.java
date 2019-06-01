f252f9b1e26bf3f9547efc31b85707cbb483fa70
andreybannikov <andrewnnov@yandex.ru>
2019-06-01 18:32:56 +0300

    GET Method and Path Variable

---
+++ resful-web-services/resful-web-services/src/main/java/com/in28/rest/websevices/resfulwebservices/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.in28.rest.websevices.resfulwebservices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + ", getMessage()=" + getMessage() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+
+}
--- resful-web-services/resful-web-services/src/main/java/com/in28/rest/websevices/resfulwebservices/HelloWorldController.java
+++ resful-web-services/resful-web-services/src/main/java/com/in28/rest/websevices/resfulwebservices/HelloWorldController.java
@@ -1,15 +1,27 @@
 package com.in28.rest.websevices.resfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class HelloWorldController {	
 	
 	@GetMapping(path ="/hello-world")
 	public String helloWorld() {
 		return "Hello world";
+	}	
+	
+	
+	//hello-world-bean
+	@GetMapping(path ="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello world Bean");
+	}	
+	
+	@GetMapping(path ="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}	
 
 }
