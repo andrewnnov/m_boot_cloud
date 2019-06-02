4e140322c6e7eef6bd17d67deeb7f9f67321dee7
andreybannikov <andrewnnov@yandex.ru>
2019-06-02 19:31:37 +0300

    internaliz

--- resful-web-services/resful-web-services/src/main/java/com/in28/rest/websevices/resfulwebservices/ResfulWebServicesApplication.java
+++ resful-web-services/resful-web-services/src/main/java/com/in28/rest/websevices/resfulwebservices/ResfulWebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -19,7 +20,7 @@ public class ResfulWebServicesApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		
-		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
 		localResolver.setDefaultLocale(Locale.US);		
 		return localResolver;		
 	}
